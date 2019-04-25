815f60b2540a88681a01b03d104226776a0e88a4
Tolin Kalin <tohopekaliga@live.com>
2019-04-25 17:41:39 -0400

    fix component dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StarSystemComponent } from './star-system/star-system.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StarSystemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
