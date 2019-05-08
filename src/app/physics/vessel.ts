17ef833fc5d056cca226d4dc7ed064c77b874260
Tolin Kalin <tohopekaliga@live.com>
2019-05-08 16:08:56 -0400

    throwing numbers at the wall before I actually do the math on this one.

--- src/app/physics/vessel.ts
+++ src/app/physics/vessel.ts
@@ -5,6 +5,7 @@ import { PointMass } from './point-mass';
 export class Vessel extends Orbiter {
   acceleration:number = 0.5;
   maxAccel: Vector2;
+  maxVel: 1; //1 AU/day is roughly 0.005 C
 
   targetBody: PointMass;
 
@@ -14,6 +15,13 @@ export class Vessel extends Orbiter {
     }
     else {
       let approachVector = this.targetBody.position.subtract(this.position);
+      let distanceSq = approachVector.magnitudeSq();
+      let velocitySq = this.velocity.magnitudeSq();
+      let impulse = this.acceleration;
+
+      if (velocitySq > distanceSq || this.maxVel * this.maxVel < velocitySq) {
+        impulse = -this.acceleration;
+      }
 
       if (approachVector.magnitudeSq() > this.acceleration * this.acceleration * dt * dt) {
         approachVector = approachVector.normalized().multiply(this.acceleration * dt);
