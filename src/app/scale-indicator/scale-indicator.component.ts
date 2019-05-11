9c5d81fe7c5d822cc1fe94730e3f7bb462bdfd8d
tohopekaliga <trisphee@gmail.com>
2019-05-10 20:03:24 -0400

    Make the Scale Indicator fancier

--- src/app/scale-indicator/scale-indicator.component.ts
+++ src/app/scale-indicator/scale-indicator.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Convert } from '../physics/math3d';
 
 @Component({
   selector: 'orb-scale-indicator',
@@ -9,16 +10,58 @@ export class ScaleIndicatorComponent {
 
   protected trueScale;
   displayScale;
+  displayValue:number = 1;
+  displayUnit:string = "AU";
+  
+  maxWidth:number = 250; //px
+  minWidth:number = 40;
 
   @Input()
   set scale(scale: number) {
     this.trueScale = scale;
-
-    this.displayScale = scale;
+    this.computeDisplay();
   }
 
   get scale(): number {
     return this.trueScale;
+  }
+
+  protected computeDisplay() {
+
+    //as the trueScale value corresponds to pixels per AU,
+    //keep the bar from getting too big or too small.
+
+    if(this.trueScale > this.maxWidth) {
+      
+      this.displayScale = this.maxWidth;
+
+      let fraction = this.displayScale / this.trueScale;
+
+      if(this.trueScale > this.maxWidth * 2) {
+        this.displayValue = Math.floor(Convert.AUtoKM(fraction));
+        this.displayUnit = "km";
+      }
+      else {
+        this.displayValue = fraction;
+        this.displayUnit = "AU";
+      }
+
+    }
+    else if(this.trueScale < this.minWidth) {
+
+      let au = Math.ceil(this.minWidth / this.trueScale);
+
+      this.displayScale = au * this.trueScale;
+
+      this.displayValue = au;
+      this.displayUnit = "AU";
+    }
+    else {
+      this.displayScale = this.trueScale;
+      this.displayValue = 1;
+      this.displayUnit = "AU";
+    }
+
   }
 
   constructor() { }
