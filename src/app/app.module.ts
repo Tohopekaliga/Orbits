8e5747756ed879e7f23c2ad659bd21679a6898b7
tohopekaliga <trisphee@gmail.com>
2019-05-11 22:38:42 -0400

    Removing references to a module I didn't actually make

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { SimSpeedBarComponent } from './sim-speed-bar/sim-speed-bar.component';
 import { ExpandingSearchBoxComponent } from './expanding-search-box/expanding-search-box.component';
 import { ScaleIndicatorComponent } from './scale-indicator/scale-indicator.component';
-import { SystemDisplayComponent } from './system-display/system-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimSpeedBarComponent,
     ExpandingSearchBoxComponent,
-    ScaleIndicatorComponent,
-    SystemDisplayComponent
+    ScaleIndicatorComponent
   ],
   imports: [
     BrowserModule//,
